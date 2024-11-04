767e2b857eb44014bca385076621f09c28eb9f05
lucid121 <166326373+lucid121@users.noreply.github.com>
2024-11-04 22:27:21 +0000

    Delete ComputerAid directory

--- ComputerAid/script/main.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".home", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  
-  navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-  },
-});
